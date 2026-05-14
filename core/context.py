8a8bdcc5b2743a1aa0cfdad176356f37b2dcf12e
Rishika Rai <22F3003067@ds.study.iitm.ac.in>
2026-05-14 15:23:55 +0530

    added Finacial context for strict variable type matching

---
+++ core/context.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel,Field
+from typing import Optional
+from datetime import datetime
+
+class FinancialContext(BaseModel):
+
+    model_config = {"strict": False, "validate_assignment": True}
+    
+    #Basic info provided by the user
+    #It is company name like "AAPL"
+    ticker : str
+    #It is full Company name like "Apple Inc"
+    company_name : str
+
+    #variables produced by financial data agents
+    #price to earnings ratio
+    pe_ratio: Optional[float] = None
+    debt_to_equity: Optional[float] = None
+    revenue_growth: Optional[float] = None
+
+    #News Agent
+    news_articles: list[str] = Field(default_factory=list)
+
+    #prduced by risk scorer
+    liquidity_risk : Optional[float] = None
+    credit_risk : Optional[float] = None
+    overall_risk : Optional[float] = None
+
+    #Contradictions agent produces
+    contradictions : list[str] = Field(default_factory=list)
+
+    #Sentiment agent produces
+    sentiment_score : Optional[float] = None
+    sentiment_signals : list[str] = Field(default_factory=list)
+
+    #Report Agents
+    final_memo : list[str] = Field(default_factory=list)
+
+    #Orchestrator Agent
+    tokens_used: int = 0
+    current_agent: Optional[str] = None
+    audit_log: list[str] = Field(default_factory=list)
+    created_at: str = Field(default_factory=lambda: datetime.now().isoformat())
+
+
+
+
